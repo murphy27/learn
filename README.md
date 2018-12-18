3b63e4dc4fb88cebe03b8984c29b2241d57e5fbb
murphy <murphy@sd.com>
2018-12-18 10:16:45 +0800

    application change

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 learn
 #date to date 
 2018-12-17
+
+tips
 加入这个包之后, 页面会自动找 static 下面的index.html 文件
 <dependency>
     <groupId>org.springframework.boot</groupId>
     <artifactId>spring-boot-starter-web</artifactId>
 </dependency>
+
+tips
+application.properties和application.yml
+可以同时存在两个文件,
+不过要以
 
 # 个人开发测试
  个人开发测试
--- src/main/java/com/yun/learn/LearnApplication.java
+++ src/main/java/com/yun/learn/LearnApplication.java
@@ -1,14 +1,30 @@
 package com.yun.learn;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
     }
+
+    @Value("${server.port}")
+    String port;
+
+    @Value("${testname}")
+    String testname;
+
+    @RequestMapping("/port")
+    public String service(){
+        return port+" "+testname;
+    }
+
 
 }
 
