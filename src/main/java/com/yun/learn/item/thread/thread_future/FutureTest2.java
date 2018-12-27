1c0c48f50b0c00a06d857cdfebfa8d0249c4439f
murphy <murphy@sd.com>
2018-12-27 10:24:35 +0800

    read me commit

--- src/main/java/com/yun/learn/item/thread/thread_future/FutureTest2.java
+++ src/main/java/com/yun/learn/item/thread/thread_future/FutureTest2.java
@@ -1,8 +1,6 @@
 package com.yun.learn.item.thread.thread_future;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -10,23 +8,22 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class FutureTest2 {
-    private static class Task implements Callable<Integer> {
+    private static class Task implements Callable<List<Map<String,Object>>> {
         @Override
-        public Integer call() throws Exception {
+        public List<Map<String,Object>> call() throws Exception {
             // 模拟真实事务的处理过程，这个过程是非常耗时的。
-            if (1==1){
-                throw new RuntimeException("隐藏报错了,,,");
-            }
+            List<Map<String,Object>> objects = new ArrayList<>();
+            Map<String,Object> map = new HashMap<>();
             Random random = new Random();
-            int back = random.nextInt(500)+200;
-            Thread.sleep(back);
-            return back;
+            map.put(random.nextInt(100)+"",random.nextInt(100));
+            objects.add(map);
+            return objects;
         }
     }
 
     public static void main(String[] args) throws InterruptedException,
             ExecutionException {
-        List<Future<Integer>> futures = new ArrayList<Future<Integer>>();
+        List<Future<List<Map<String,Object>>>> futures = new ArrayList<Future<List<Map<String,Object>>>>();
         ExecutorService executorService = Executors.newCachedThreadPool();
 
         System.out.println("已经提交资源申请");
@@ -34,7 +31,7 @@ public class FutureTest2 {
             futures.add(executorService.submit(new Task()));
         }
         int times = 1;
-        for (Future<Integer> future : futures) {
+        for (Future<List<Map<String,Object>>> future : futures) {
             // 判断资源是不是已经准备完毕，准备完毕直接获取。
             int i=1;
             while (!future.isDone()) {
